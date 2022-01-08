c4a2c5707546d72755f4311296b714f45bd08531
mhmdrameez <muhammedrameez2000@gmail.com>
2022-01-08 16:57:22 +0530

    ok

---
+++ setup.sh
@@ -0,0 +1,10 @@
+ $gedit setup.sh
+  #Copy the code below
+	      mkdir -p ~/.streamlit/
+	      echo "\
+              [server]\n\
+              headless = true\n\
+              port = $PORT\n\
+              enableCORS = false\n\
+              \n\
+              " > ~/.streamlit/config.toml
